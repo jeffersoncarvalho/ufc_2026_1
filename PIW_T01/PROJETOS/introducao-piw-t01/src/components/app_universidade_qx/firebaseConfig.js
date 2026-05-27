206254c3a338ef85f7a6f671a1cbd4cf57dc2dfa
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2026-05-27 08:10:58 -0300

    firebase

--- PIW_T01/PROJETOS/introducao-piw-t01/src/components/app_universidade_qx/firebaseConfig.js
+++ PIW_T01/PROJETOS/introducao-piw-t01/src/components/app_universidade_qx/firebaseConfig.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD-KKx6IfiBB8ChlPhAW2YWovtkoa-3q4M",
-  authDomain: "jeff-piwt01.firebaseapp.com",
-  projectId: "jeff-piwt01",
-  storageBucket: "jeff-piwt01.firebasestorage.app",
-  messagingSenderId: "931650123312",
-  appId: "1:931650123312:web:758e69ebc83c1af773dff5"
-};
+
 
 const app = initializeApp(firebaseConfig) //criou a aplicação
 const dbFirestore = getFirestore(app)
